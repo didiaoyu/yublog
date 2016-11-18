9591f117433c2ad381bf7b997181fdced8f6de90
lixiaoyu <lxy1001@163.com>
2016-11-18 21:21:55 +0800

    修改表名称

---
+++ app/Model/Admin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    protected $table = 'admin';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'email', 'password',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
