aec78cec8216d3b1f638b829469a034876e2ca4f
李晓雨 <lixy@weipaitang.com>
2017-04-21 16:34:09 +0800

    测试控制器

--- public/test.php
+++ public/test.php
@@ -5,9 +5,9 @@
  * Date: 2017/4/21
  * Time: 下午4:29
  */
-if (isset($_POST)) {
+if (!empty($_POST)) {
     echo 'post';
-} elseif (isset($_GET)) {
+} elseif (!empty($_GET)) {
     echo 'get';
 } else {
     echo 'unknow method';
