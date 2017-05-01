f6e19793134105173c39eac15d2fbd9558dc9118
李晓雨 <lixy@weipaitang.com>
2017-05-01 22:17:28 +0800

    增加cnzz异步统计代码

--- resources/views/default/layouts/main.blade.php
+++ resources/views/default/layouts/main.blade.php
@@ -68,6 +68,15 @@
 <script src="/assets/vendor/js/bootstrap.min.js"></script>
 {{--<script src="js/site.js"></script>--}}
 @yield('page_script')
+<script>
+    var cnzz_s_tag = document.createElement('script');
+    cnzz_s_tag.type = 'text/javascript';
+    cnzz_s_tag.async = true;
+    cnzz_s_tag.charset = 'utf-8';
+    cnzz_s_tag.src = 'http://w.cnzz.com/c.php?id=1261852234&async=1';
+        var root_s = document.getElementsByTagName('script')[0];
+    root_s.parentNode.insertBefore(cnzz_s_tag, root_s);
+</script>
 </body>
 
 </html>
